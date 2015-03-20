7fb177b123fae97c0029f8bc977b4dcaad7b5e94
Zsolt Gál <zsolt.gal@sessiondigital.com>
2015-03-20 01:22:27 +0100

    Display basic info on list, allow loading last email

--- src/controllers/RestController.php
+++ src/controllers/RestController.php
@@ -7,15 +7,27 @@ class Zenc_EmailLogger_RestController extends Zenc_EmailLogger_Controller_Restfu
     public function getListAction()
     {
         $this->render(array(
-            'count' => $this->_getCollection()->count()
+            'count' => $this->_getCollection()->count(),
+            'items' => $this->_getCollection()->walk(function($item) {
+                return array(
+                    'id' => $item->getId(),
+                    'subject' => $item->getSubject(),
+                    'created_at' => $item->getCreatedAt(),
+                );
+            })
         ));
     }
 
     public function getReadAction()
     {
-        $log = Mage::getModel('zenc_emaillogger/log')->load($this->getRequest()->get('id'));
+        $id = $this->getRequest()->get('id');
+        if ($id == 'last') {
+            $item = $this->_getCollection()->getLastItem();
+        } else {
+            $item = Mage::getModel('zenc_emaillogger/log')->load($id);
+        }
 
-        $this->render($log->getData());
+        $this->render($item->getData());
     }
 
     private function _getCollection()
