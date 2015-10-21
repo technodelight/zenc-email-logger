45c91a5218f6f88aae1b5e3b4de716d48fd8e29c
Zsolt Gál <zsolt.gal@sessiondigital.com>
2015-10-21 10:32:29 +0200

    Add adminhtml controller and system menu

---
+++ src/controllers/Adminhtml/EmailLoggerController.php
@@ -0,0 +1,13 @@
+<?php
+
+use Mage_Adminhtml_Controller_Action as AdminController;
+
+class Zenc_EmailLogger_Adminhtml_EmailLoggerController extends AdminController
+{
+
+    public function indexAction()
+    {
+
+    }
+
+}
